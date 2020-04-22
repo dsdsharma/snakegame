45616406485cc21d4f8f5eae1430dd3c795ae538
Deepak Sharma <dev2sharma2k@gmail.com>
2020-04-23 01:18:01 +0530

    Made Snake moving
    Snake can move and change directions

--- lib/components/snake.dart
+++ lib/components/snake.dart
@@ -1,19 +1,30 @@
 import 'dart:ui';
 import 'package:snakegame/sketch.dart';
-import 'package:snakegame/helpers/directions.dart';
-import 'dart:math';
 
 class Snake {
-  Direction direction = Direction.up;
+  List<double> dx;
+  List<double> dy;
+  int rez = 10;
+
+  double xdir;
+  double ydir;
   final Sketch game;
   Offset position = Offset.zero;
   Snake(this.game, {this.position});
 
+  // this.dx[0] = 0.0;
+  //this.dy[0] = 0.0;
+  //this.body[0] = Offset(0,0);
+  // this.xdir = 0;
+  //this.ydir = 0;
+  // print('Value of offset dsdddddddddd'+(body[0].dx).toString());
+
   void render(Canvas c) {
     Paint snakeColor = Paint()..color = Color(0xffdddddd);
     c.save();
+
     c.translate(position.dx, position.dy);
-    Rect snake = Rect.fromLTWH(0, 0, 30, 30);
+    Rect snake = Rect.fromLTWH(position.dx, position.dy, 20, 20);
 
     c.drawRect(snake, snakeColor);
     //TEST: Moving by on unit in particular direction
--- lib/sketch.dart
+++ lib/sketch.dart
@@ -4,30 +4,30 @@ import 'package:flame/flame.dart';
 import 'package:flutter/gestures.dart';
 
 import 'package:snakegame/components/snake.dart';
-import 'package:snakegame/helpers/directions.dart';
 
 class Sketch extends Game {
   String lastMove = '';
-  double xMovement = 0;
+  double xMovement = 1;
   double yMovement = 0;
   Snake snake;
   Size screenSize;
+  List<Offset> body = List<Offset>();
   Sketch() {
+    body.add(Offset(0, 0));
     initialize();
   }
+
   void initialize() async {
     resize(await Flame.util.initialDimensions());
-    if (snake == null) {
-      snake = Snake(
-        this,
-        position: Offset(screenSize.width / 2, screenSize.height / 2),
-      );
-    }
+
+    snake = Snake(
+      this,
+      position: body[0],
+    );
   }
 
   @override
   void render(Canvas canvas) {
-    // TODO: implement render
     Rect bgRect = Rect.fromLTWH(0, 0, screenSize.width, screenSize.height);
     Paint bgPaint = Paint();
 
@@ -38,70 +38,62 @@ class Sketch extends Game {
 
   @override
   void update(double t) {
-    // TODO: implement update
-    // make snake move
-    if (lastMove == 'x') {
-      if (xMovement < 0) {
-        snake.direction = Direction.left;
-        snake.position = snake.position.translate(-100 * t, 0);
-      }
-      if (xMovement > 0) {
-        snake.direction = Direction.right;
-        snake.position = snake.position.translate(100 * t, 0);
-      }
-    }
-    if (lastMove == 'y') {
-      if (yMovement < 0) {
-        snake.direction = Direction.up;
-        snake.position = snake.position.translate(0, -100 * t);
-      }
-      if (yMovement > 0) {
-        snake.direction = Direction.down;
-        snake.position = snake.position.translate(0, 100 * t);
-      }
-    }
+    this.body[0] += Offset(xMovement, yMovement);
+    snake.position += Offset(xMovement,yMovement);
+
+
+    // Code ment to rotate the object on its axis
+    // if (lastMove == 'x') {
+    //   if (xMovement < 0) {
+    //     snake.direction = Direction.left;
+    //     snake.position = snake.position.translate(-100 * t, 0);
+    //   }
+    //   if (xMovement > 0) {
+    //     snake.direction = Direction.right;
+    //     snake.position = snake.position.translate(100 * t, 0);
+    //   }
+    // }
+    // if (lastMove == 'y') {
+    //   if (yMovement < 0) {
+    //     snake.direction = Direction.up;
+    //     snake.position = snake.position.translate(0, -100 * t);
+    //   }
+    //   if (yMovement > 0) {
+    //     snake.direction = Direction.down;
+    //     snake.position = snake.position.translate(0, 100 * t);
+    //   }
+    // }
   }
 
   @override
   void resize(Size size) {
-    // TODO: implement resize
     super.resize(size);
     screenSize = size;
   }
 
-  void onUpDragStart(DragDownDetails d) {
-    yMovement += -1;
-    lastMove = 'y';
-  }
 
-  void onUpDragEnd(DragEndDetails d) {
-    yMovement += 1;
+  //Functions setting value for direction change
+  void onUpDragStart(DragDownDetails d) {
+    xMovement = 0;
+    yMovement = -1;
+    lastMove = 'y';
   }
 
   void onRightDragStart(DragDownDetails d) {
-    xMovement += 1;
+    xMovement = 1;
+    yMovement=0;
     lastMove = 'x';
-  }
-
-  void onRightDragEnd(DragEndDetails d) {
-    xMovement += -1;
   }
 
   void onDownDragStart(DragDownDetails d) {
-    yMovement += 1;
+    xMovement = 0;
+    yMovement = 1;
     lastMove = 'y';
   }
 
-  void onDownDragEnd(DragEndDetails d) {
-    yMovement += -1;
-  }
-
   void onLeftDragStart(DragDownDetails d) {
-    xMovement += -1;
+    xMovement = -1;
+    yMovement = 0;
     lastMove = 'x';
-  }
-
-  void onLeftDragEnd(DragEndDetails d) {
-    xMovement += 1;
   }
 }
